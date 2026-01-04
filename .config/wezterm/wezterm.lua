ea0d2bdc79c964b996fcb3e1e91f61150b5aabbd
Stefan Loibl <loibl.stefan@gmail.com>
2026-01-04 21:09:36 +0100

    make wezterm wayland compatible

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,8 +1,9 @@
 local wezterm = require 'wezterm'
 return {
-	color_scheme = 'Gruvbox Material (Gogh)',
-	hide_tab_bar_if_only_one_tab = true,
-	font_size = 14.0,
-	font = wezterm.font('JetBrains Mono'),
-	window_background_opacity = 1.0,
+  color_scheme = 'Gruvbox Material (Gogh)',
+  hide_tab_bar_if_only_one_tab = true,
+  font_size = 14.0,
+  font = wezterm.font('JetBrains Mono'),
+  window_background_opacity = 1.0,
+  enable_wayland = false,
 }
